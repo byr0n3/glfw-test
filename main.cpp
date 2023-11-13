32f93e65b7f33beeab02a3d59273b12c5c2fd058
Byron <byronoud10@gmail.com>
2023-11-13 16:51:47 +0100

    drawing cube, vertex colors, performance metrics

--- main.cpp
+++ main.cpp
@@ -3,13 +3,74 @@
 #include <GLFW/glfw3.h>
 #include <glm/gtc/matrix_transform.hpp>
 #include <include/shaders.h>
+#include <sstream>
 
 // basic triangle shape
-static const GLfloat triangle_vertex[] = {
-		-1.0f, -1.0f, 0.0f,
-		1.0f, -1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
+static const GLfloat cube_vertex[] = {
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, 1.0f, -1.0f,
+		1.0f, -1.0f, 1.0f,
+		-1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, 1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, -1.0f,
+		1.0f, -1.0f, 1.0f,
+		-1.0f, -1.0f, 1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, -1.0f, 1.0f,
+		1.0f, -1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, 1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, -1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, -1.0f,
+		-1.0f, 1.0f, -1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, -1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, -1.0f, 1.0f
 };
+
+static GLfloat colors[12 * 3 * 3];
+
+static double lastRenderTime;
+static int frameCount;
+
+void updatePerformanceMetrics(GLFWwindow *window) {
+	double currentTime = glfwGetTime();
+	double delta = currentTime - lastRenderTime;
+	frameCount++;
+
+	if (delta >= 1.0) {
+		auto frameTime = 1000.0 / double(frameCount);
+		auto fps = int(1000 / frameTime);
+
+		std::stringstream temp;
+		temp << fps << " FPS | " << frameTime << "ms";
+
+		glfwSetWindowTitle(window, temp.str().c_str());
+		frameCount = 0;
+		lastRenderTime += 1.0;
+	}
+}
+
+float rnd_color() {
+	return (float) std::rand() / (float) RAND_MAX;
+}
 
 int main() {
 	if (!glfwInit()) {
@@ -17,7 +78,8 @@ int main() {
 		return 1;
 	}
 
-	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	// 4x antialiasing
+	glfwWindowHint(GLFW_SAMPLES, 4);
 
 	// OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -50,6 +112,10 @@ int main() {
 	// Prevents missing state changes by only resetting them when `glfwGetKey` gets called
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+	// Enable the Z-Buffer to only render triangles that we should renderit
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
 	// Create a blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
@@ -70,9 +136,9 @@ int main() {
 
 	// Camera view
 	glm::mat4 cameraView = glm::lookAt(
-			// Camera is positioned at (4,3,3)
-			glm::vec3(4, 3, 3),
-			// Camera looks at the origin
+			// Camera position
+			glm::vec3(4, 3, -3),
+			// Camera rotation/angle
 			glm::vec3(0, 0, 0),
 			// And looks up (Vector3.up)
 			glm::vec3(0, 1, 0)
@@ -81,7 +147,7 @@ int main() {
 	// Model matrix : an identity matrix (model will be at the origin)
 	auto model = glm::mat4(1.0f);
 
-	// Model view projection : multiplication of our 3 matrices
+	// Model view projection = multiplication of our 3 matrices
 	glm::mat4 mvp = projection * cameraView * model;
 
 	GLuint vertexBuffer;
@@ -90,12 +156,29 @@ int main() {
 	// Use our vertex buffer
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 	// Give the triangle vertices
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertex), triangle_vertex, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertex), cube_vertex, GL_STATIC_DRAW);
+
+	GLuint colorBuffer;
+	glGenBuffers(1, &colorBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+
+	lastRenderTime = glfwGetTime();
+	frameCount = 0;
 
 	// Listen for the 'Escape' key for closing
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0) {
+		updatePerformanceMetrics(window);
+
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		for (int i = 0; i < 12 * 3; i++) {
+			colors[3 * i] = rnd_color();
+			colors[3 * i + 1] = rnd_color();
+			colors[3 * i + 2] = rnd_color();
+		}
+
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 
 		// Use our compiled shaders
 		glUseProgram(programId);
@@ -115,8 +198,19 @@ int main() {
 				(void *) nullptr // offset
 		);
 
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+		glVertexAttribPointer(
+				1,
+				3,
+				GL_FLOAT,
+				GL_FALSE,
+				0,
+				(void *) nullptr
+		);
+
 		// Draw
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
 		glDisableVertexAttribArray(0);
 
 		// Swap the buffer with the new one
